fe91515b26d36aa46055e7cbc5208cd8441ce33b
girishgodage <girishgodage@gmail.com>
2018-07-03 07:33:16 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.get('/',(req,res) => {
+    res.send('Hello Story Books Apps');
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,() => {
+    console.log(`Server started on Port ${port}`);
+});
