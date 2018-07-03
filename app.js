8b97b586861ddc23a6aba1357c2c2865ee414a83
girishgodage <girishgodage@gmail.com>
2018-07-03 10:51:33 +0530

    completed Google authentication

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
 const app = express();
 
+// Load User Model
+require('./models/User');
+
 // Passport Config
 require('./config/passport')(passport);
-
 
 // Load routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+    useMongoClient:true
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/',(req,res) => {
     res.send('Hello Story Books Apps');
 })
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+  });
 
 // Use routes
 app.use('/auth', auth);
