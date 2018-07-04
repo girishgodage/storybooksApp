cd2b9241aa5e3262cf6a7e226ca49e2e84c26740
girishgodage <girishgodage@gmail.com>
2018-07-04 12:06:16 +0530

    Version 1.0.0 Alpha

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports ={
-    mongoURI: 'mongodb://girish:g1r1sh@ds125021.mlab.com:25021/giri-storybooks-dev',
-    googleClientID: '87079222550-a0cava2trs22segblr05ov7jcu405ui1.apps.googleusercontent.com',
-    googleClientSecret : '_nGWYWTR1osyx0y4g-KhC8ZD'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
